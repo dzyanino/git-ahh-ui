d45783e1e045ee71e46ff79552d0f540f5489ada
dzyanino <dzyanino@gmail.com>
2026-05-25 17:58:24 +0300

    feat: added theme colors from the design

---
+++ include/theme/colors.c
@@ -0,0 +1,13 @@
+#include "colors.h"
+
+Clay_Color theme_colors[] = {
+ [M_COLOR_PRIMARY] = { 242, 194, 48, 255 },
+ [M_COLOR_SECONDARY] = { 70, 51, 79, 255 },
+ [M_COLOR_ACCENT] = { 242, 79, 19, 255 },
+ [M_COLOR_BACKGROUND] = { 128, 130, 166, 255 },
+ [M_COLOR_DARK_PRIMARY] = { 242, 146, 29, 255 },
+ [M_COLOR_DARK_BACKGROUND] = { 104, 106, 148, 255 },
+ [M_COLOR_TEXT_WHITE] = { 223, 224, 232, 255 },
+ [M_COLOR_TEXT_DIM] = { 186, 187, 207, 255 },
+ [M_COLOR_BORDER] = { 146, 148, 178, 255 },
+};
