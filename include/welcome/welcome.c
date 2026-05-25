0233b5a57ba3d11d0c0f033d1fd43c54e62a470a
dzyanino <dzyanino@gmail.com>
2026-05-25 20:28:38 +0300

    chore: update ui

--- include/welcome/welcome.c
+++ include/welcome/welcome.c
@@ -19,21 +19,29 @@ void LayoutWelcome() {
     }
   ) {
     CLAY(
-      CLAY_ID("empty-card"),
+      CLAY_ID("empty-state-card"),
       {
         .layout = {
+          .layoutDirection = CLAY_TOP_TO_BOTTOM,
           .sizing = {
             .width = CLAY_SIZING_FIXED(400),
             .height = CLAY_SIZING_FIXED(200)
           },
+          .childAlignment = {
+            .x = CLAY_ALIGN_X_CENTER
+          },
+          .childGap = 4,
           .padding = CLAY_PADDING_ALL(16)
         },
-        .backgroundColor = {128, 130, 166, 255},
-        .cornerRadius = 10
+        .backgroundColor = theme_colors[M_COLOR_BACKGROUND],
+        .cornerRadius = 5
       }
     ) {
-      CLAY_TEXT(CLAY_STRING("Ok test leka"), {
+      CLAY_TEXT(CLAY_STRING("There is no repository here"), {
         .fontId = M_FONT_DISPLAY_BOLD, .fontSize = 24, .textColor = theme_colors[M_COLOR_TEXT_WHITE]
+      });
+      CLAY_TEXT(CLAY_STRING("Open one to get started"), {
+        .fontId = M_FONT_REGULAR, .fontSize = 18, .textColor = theme_colors[M_COLOR_TEXT_DIM]
       });
     }
   }
