dc0fb1f7fd0f60f5f98bc3f6556a537334a7220a
dzyanino <dzyanino@gmail.com>
2026-06-03 09:54:34 +0300

    feat(button): added variants to button with `disabled` state handled #3

--- include/components/button/button.c
+++ include/components/button/button.c
@@ -3,17 +3,18 @@
 #include "../../theme/fonts.h"
 #include "../../utils/clay_string.h"
 #include <raylib.h>
-#include <stdio.h>
 
 void HandleClick(Clay_ElementId element_id, Clay_PointerData pointer_info, void *user_data) {
   if (pointer_info.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME) {
-    printf("Nice it works\n");
     void (*callback)() = user_data;
     if (callback) callback();
   }
 }
 
-void ComponentButton(char* id, char* label, int variant, m_color_id_t color_id, int* disabled, void (*callback)()) {
+void ComponentButton(char* id, char* label, m_variant_id_t variant_id, m_color_id_t color_id, int* disabled, void (*callback)()) {
+  bool is_outline = variant_id == M_VARIANT_OUTLINE;
+  bool is_disabled = *disabled;
+
   CLAY(
     CLAY_SID(DynamicClayString(id)),
     {
@@ -30,23 +31,50 @@ void ComponentButton(char* id, char* label, int variant, m_color_id_t color_id, 
         .layoutDirection = CLAY_LEFT_TO_RIGHT,
         .padding = {16, 16, 8, 8}
       },
-      .cornerRadius = 5,
+
+      .cornerRadius = (Clay_CornerRadius) {5, 5, 5, 5},
+
+      .border = is_outline
+        ? (Clay_BorderElementConfig) {
+            .width = CLAY_BORDER_ALL(2),
+            .color = is_disabled ? theme_colors[M_COLOR_BORDER] : theme_colors[color_id]
+          }
+        : is_disabled
+          ? (Clay_BorderElementConfig) {
+              .width = CLAY_BORDER_ALL(2),
+              .color = theme_colors[M_COLOR_BORDER]
+            }
+          : (Clay_BorderElementConfig) {0},
+
       .backgroundColor = Clay_Hovered()
-        ? (*disabled)
-          ? (SetMouseCursor(MOUSE_CURSOR_NOT_ALLOWED), theme_colors[M_COLOR_BORDER])
-          : (SetMouseCursor(MOUSE_CURSOR_POINTING_HAND), theme_colors[M_COLOR_DARK_PRIMARY])
-        : (*disabled)
-          ? theme_colors[M_COLOR_BORDER]
-          : theme_colors[color_id]
+        ? is_disabled
+          ? (
+              SetMouseCursor(MOUSE_CURSOR_NOT_ALLOWED),
+              is_outline
+                ? (Clay_Color){0, 0, 0, 0}
+                : theme_colors[M_COLOR_BORDER]
+            )
+          : is_outline
+            ? (SetMouseCursor(MOUSE_CURSOR_POINTING_HAND), theme_colors[M_COLOR_BORDER])
+            : (SetMouseCursor(MOUSE_CURSOR_POINTING_HAND), theme_colors[M_COLOR_DARK_PRIMARY])
+        : is_disabled
+          ? is_outline
+            ? (Clay_Color){0, 0, 0, 0}
+            : theme_colors[M_COLOR_BORDER]
+          : is_outline
+            ? (Clay_Color){0, 0, 0, 0}
+            : theme_colors[color_id]
     }
   ) {
-    if (!(*disabled)) Clay_OnHover(HandleClick, callback);
+    if (!is_disabled) Clay_OnHover(HandleClick, callback);
     CLAY_TEXT(DynamicClayString(label), {
       .fontId = M_FONT_SEMIBOLD,
       .fontSize = 18,
-      .textColor = (*disabled)
+      .textColor = is_disabled
         ? theme_colors[M_COLOR_TEXT_DIMMED]
-        : theme_colors[M_COLOR_TEXT_WHITE]
+        : is_outline
+          ? theme_colors[color_id]
+          : theme_colors[M_COLOR_TEXT_WHITE]
     });
   }
 }
--- include/components/button/button.h
+++ include/components/button/button.h
@@ -2,9 +2,10 @@
 #define BUTTON_H
 
 #include "../../theme/colors.h"
+#include "../../theme/variants.h"
 
 void HandleClick(Clay_ElementId element_id, Clay_PointerData pointer_info, void *user_data);
 
-void ComponentButton(char* id, char* label, int variant, m_color_id_t color_id, int* disabled, void (*callback)());
+void ComponentButton(char* id, char* label, m_variant_id_t variant_id, m_color_id_t color_id, int* disabled, void (*callback)());
 
 #endif
